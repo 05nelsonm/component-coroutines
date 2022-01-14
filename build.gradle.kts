50acc0aec20fc9b347dba5498cdb0f7d64c202fb
Matthew Nelson <developer@matthewnelson.io>
2022-01-14 10:57:28 -0500

    remove unnecessary plugins/configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,3 @@
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
-import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
-import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
-import org.gradle.api.tasks.testing.logging.TestLogEvent.STARTED
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
 
@@ -16,8 +10,6 @@ buildscript {
     dependencies {
         classpath(io.matthewnelson.kotlin.components.dependencies.plugins.android.gradle)
         classpath(io.matthewnelson.kotlin.components.dependencies.plugins.kotlin.gradle)
-//        classpath(io.matthewnelson.kotlin.components.dependencies.plugins.kotlin.dokka)
-        classpath(io.matthewnelson.kotlin.components.dependencies.plugins.intellij)
         classpath(io.matthewnelson.kotlin.components.dependencies.plugins.mavenPublish)
 
         // NOTE: Do not place your application dependencies here; they belong
@@ -31,14 +23,6 @@ allprojects {
         mavenCentral()
         google()
         gradlePluginPortal()
-    }
-
-    tasks.withType<Test> {
-        testLogging {
-            exceptionFormat = TestExceptionFormat.FULL
-            events(STARTED, PASSED, SKIPPED, FAILED)
-            showStandardStreams = true
-        }
     }
 
 }
