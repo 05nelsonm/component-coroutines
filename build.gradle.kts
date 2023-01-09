99356ab6d42ffd7214abf2ec7c2445155cb09db5
Matthew Nelson <developer@matthewnelson.io>
2023-01-09 04:06:41 -0500

    Update kotlin-components submodule to latest commit (dependency + build updates)

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,8 +26,8 @@ buildscript {
     }
 
     dependencies {
-        classpath(io.matthewnelson.kotlin.components.dependencies.plugins.kotlin.gradle)
-        classpath(io.matthewnelson.kotlin.components.dependencies.plugins.mavenPublish)
+        classpath(pluginDeps.kotlin.gradle)
+        classpath(pluginDeps.mavenPublish)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
